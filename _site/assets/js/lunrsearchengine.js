ea30f948a2cf57057cd7e47e4402465c8a4042c4
Durwasa Chakraborty <durwasa@Durwasas-MacBook-Pro.local>
2019-06-01 20:54:03 -0400

    first commit

---
+++ _site/assets/js/lunrsearchengine.js
@@ -0,0 +1,104 @@
+
+var documents = [{
+    "id": 0,
+    "url": "http://localhost:4000/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "http://localhost:4000/about",
+    "title": "About Me",
+    "body": "My name is Durwasa Chakraborty and I am currently pursuing my Masters in  Computer Science with my current concentration partly in Software  Engineering, Programming Language and partly in Computer Graphics. I am extremely passionate about programming languages and their behaviors  in different settings. Yet I don’t have a favorite programming language  as I really like Python but at the same time I really appreciate the  power of Haskell or Racket. I like to work on technologies that are  close to the metal. At the same time I like to build toy projects and  fiddle around Python, JavaScript. I am very passionate about open source development, programming design paradigms, application architecture and system designs. In the past I have done some competitive coding as well so I understand algorithms. In fact, one of my life goal is to become a TopCoder purple. It might take like 20 years from now but I hope I reach there. Along with that I have worked with Opencv, I have worked with Computer Graphics, Information Retrieval and some basic web stuffs. I also have tried a plethora of environments but I couldn’t find the one IDE until I came to Spacemacs.  So, I have tried vim, np++, emacs, neovim, sublime text, Intellij (huge fan) and VSCode. I usually code in Emacs these days but then for HTML and stuffs I use VSCode (which is pretty awesome as well)I have worked for IBM for about a year in Kolkata, West Bengal as an Application Developer for SAP HANA Business Warehousing. I am currently working as a Software Engineering Intern at refactored. ai as a Django Developer . I have thoroughly enjoyed by previous job as a teaching assistant for CS3650 Computer Systems and honestly, I really like my current job, so much that I am now thinking seriously about getting a PhD but then it is a distant dream. Basically, in my 5 years of Computer Science education I have practically tried on everything but I have failed miserably to get all my ducks in a row. Somewhere, I do feel the dire need to specialize in one of the fields but then I like it this way. "
+    }, {
+    "id": 2,
+    "url": "http://localhost:4000/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "http://localhost:4000/",
+    "title": "Home",
+    "body": "      Featured:                           All Stories:                                                                                                     The Glass Wall              :       A fantastic year comes to an end like many others but I still remember the one year in which an entourage of extraordinary human beings had gathered around me. I. . . :                                                                               Durwasa                12 Mar 2019                                                                                                                                    Powerful things you can do with the Markdown editor              :       There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips. . . :                                                                               Durwasa                12 Jun 2018                                            "
+    }, {
+    "id": 4,
+    "url": "http://localhost:4000/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 5,
+    "url": "http://localhost:4000/the-glass-wall/",
+    "title": "The Glass Wall",
+    "body": "2019/03/12 - A fantastic year comes to an end like many others but I still remember the one year in which an entourage of extraordinary human beings had gathered around me. I just hoped that our story might have a good ending but sometimes wishes don’t come true. Staring at the Glass Wall I see myself standing right there alone and carrying a whole bag of despair and endless remorse but then I had sown the seeds and now I witness the repercussions . But my story was not like this at all. There was a time when I saw her in my reflection. I could see her enchanting beauty refracted quite meticulously by her pair of glasses. I could actually see a lot of affection in those eyes no matter how small my angle of incidence was the image that I received was larger than life. Deep down I never could have calculated that someday my angle would reduce to the critical angle and I would be pushed to eternal blackness. My story started with a plasma wall colloquially known as the “monitor” or “personal computer” when I came to know about this wonderful person. I really don’t know when I came close to her and suddenly some streams of binary bits connected us. Those streams of binary bits started to make sense and I still wonder how simple subtle series of zeros and ones could affect my life. It still does. . At times I just stare at the same plasma wall trying to read from the past conversations. The color of the wall has faded away because there’s no color at all. It doesnt make any sense now to read things that have been my colorful and radiant past.  But I read it still some paragraphs I have read it soo many times that I just need no wall to read them. Comes naturally… as always… Beginning of my life I have chased many dreams some did come true but majorly things failed. When dreams fail this glass wall in which I see myself at some point of time it just comes crashing down with the pieces hitting straight to me. Blistering me with the pointed particles leaving me asphyxiated at times. I can feel the warmth of blood oozing out from my body along with the energy. Finally a day when I lost everything I found myself drowning in the ocean of melancholy. I can’t scream I will lose my breathe I can’t make signs as no one can see me and suddenly I found myself in a much more comfortable position. All bad dreams come to an end and so was mine… I could feel the tender touch of someone right on my forehead and that belief that the world has not ended. Everybody is haunted by their pasts but some have a haunted story as well. Reaching the zenith they see their problems. Laugh at the minuscule size of their problems and tread ahead for something more , each inch fuels them for more kilometers. I came to know about her.  Slowly and steadily I could wipe the mist of my glass wall and I saw her right as my reflection. I had found the girl of my lifetime. I was touched by her solicitude. Suddenly love ballads and romantic literature started to make sense. I started to dream again and I got life my life back. I wonder someone placed a talisman right under my pillow because finding such a person was sheer good luck. One day my reflection started to fade away from me. I could see her going I couldn’t do anything. I tried but the cracks had become crevasses. She started to fill the crack but she bled with the sharp cut. I never wanted this to happen to her. I wanted the best for her. I ultimately failed ,the crack is still there; my reflection is now distorted. But I was feeling obliged about the fact that in this life I was loved by her. I left a silent tear and a wishful request. I was sure that its impossible for her and me to converge at some other point of space and time curve this was my last chance. The chill of winter actually froze me and infinitely worse deep inside I went cold. Couple of days later I was back to normal. Hoping things could get better but it did not help. Now after so many years I see her. I had always wished for a healthy prologue and a stronger epilogue. My reflection was back. I scrounged for my glass wall everywhere but I couldn’t find it anywhere. Some pieces I found in my attic the image still being distorted. But now I see the love of my life. Yet again I wished my reflection would identify me , imitate me laterally inverted although but in coherence. A bonding that a man shares with his shadow but a bonding that is colorful at both the ends.  Shes happier now …Married, successful having degrees from Ivy leagues and couple of beautiful kids. I wished to rush to the other side of the road and may be talk to her for couple of minutes. I wish to scream and shout and tell her that miss I still love you beyond all measures beyond all realms and still I do. But I didn’t or rather I could not. Not because I was afraid that what society will talk. Because I just wanted to live that dream once she had. Because do something as simple as holding her hand and telling her that I would never let her go. But I still did not or rather I would say I could not because long ago I had left this world… "
+    }, {
+    "id": 6,
+    "url": "http://localhost:4000/powerful-things-markdown-editor/",
+    "title": "Powerful things you can do with the Markdown editor",
+    "body": "2018/06/12 - There are lots of powerful things you can do with the Markdown editor. If you’ve gotten pretty comfortable with writing in Markdown, then you may enjoy some more advanced tips about the types of things you can do with Markdown! As with the last post about the editor, you’ll want to be actually editing this post as you read it so that you can see all the Markdown code we’re using. Special formatting: As well as bold and italics, you can also use some other special formatting in Markdown when the need arises, for example:  strike through ==highlight== *escaped characters*Writing code blocks: There are two types of code elements which can be inserted in Markdown, the first is inline, and the other is block. Inline code is formatted by wrapping any word or words in back-ticks, like this. Larger snippets of code can be displayed across multiple lines using triple back ticks: 123. my-link {  text-decoration: underline;}HTML: 12345&lt;li class= ml-1 mr-1 &gt;  &lt;a target= _blank  href= # &gt;  &lt;i class= fab fa-twitter &gt;&lt;/i&gt;  &lt;/a&gt;&lt;/li&gt;CSS: 12345678. highlight . c {  color: #999988;  font-style: italic; }. highlight . err {  color: #a61717;  background-color: #e3d2d2; }JS: 123456789// alertbar later$(document). scroll(function () {  var y = $(this). scrollTop();  if (y &gt; 280) {    $('. alertbar'). fadeIn();  } else {    $('. alertbar'). fadeOut();  }});Python: 1print( Hello World )Ruby: 123require 'redcarpet'markdown = Redcarpet. new( Hello World! )puts markdown. to_htmlC: 1printf( Hello World ); Reference lists: The quick brown jumped over the lazy. Another way to insert links in markdown is using reference lists. You might want to use this style of linking to cite reference material in a Wikipedia-style. All of the links are listed at the end of the document, so you can maintain full separation between content and its source or reference. Full HTML: Perhaps the best part of Markdown is that you’re never limited to just Markdown. You can write HTML directly in the Markdown editor and it will just work as HTML usually does. No limits! Here’s a standard YouTube embed code as an example: "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
